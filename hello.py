226099d0a52eec2bcf315085701d0b983239156e
DR69 <pj350284.pj@gmail.com>
2021-07-06 11:44:40 +0530

    modified hello_world

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print('hello_world')
 print('how are you??')
+print('Espressif provides basic hardware and software resources to help application developers realize their ideas using the ESP32 series hardware. The software development framework by Espressif is intended for development of Internet-of-Things (IoT) applications with Wi-Fi, Bluetooth, power management and several other system features.')
